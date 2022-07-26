26cc0fa849f8dbef8fa312e8691a2dc5d1a1856c
Kostya <kostya.mikhalkov@bk.ru>
2022-07-26 18:31:03 +0300

    MNTN

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+
+new WOW().init();
